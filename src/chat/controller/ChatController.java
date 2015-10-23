a4f476c861c6a83da2be3ccbae2bbafdf50c6330
Colm Laro <smartman97@gmail.com>
2015-10-23 09:23:51 -0600

    Allows user to give name

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,19 +1,28 @@
 package chat.controller;
 
+import chat.model.Chatbot;
 import chat.view.ChatView;
 
 /**
  * Application controller for the ChatBot project.
  * @author clar5254
- *@version 1.1 10/21/15 added a reference to display
+ *@version 1.2 10/23/15 
  */
 
 public class ChatController
 {
 	private ChatView myDisplay;
+	private Chatbot myBot;
+	
+	public ChatController()
+	{
+		myDisplay = new ChatView();
+		String userName = myDisplay.grabAnswer("What is your name?");
+		myBot = new Chatbot(userName);
+	}
 	
 	public void start()
 	{
-		
+		myDisplay.showResponse("Hello " + myBot.getUserName());
 	}
 }
--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Base version of the 2015 Chatbot class. Only stub methods are provided. Students will complete methods as part
  * of the project.
  * @author Colm Laro
- * @version 1.0 10/21/15
+ * @version 1.2 10/23/15
  */
 public class Chatbot
 {
@@ -21,7 +21,7 @@ public class Chatbot
 	 */
 	public Chatbot(String userName)
 	{
-		
+		this.userName = userName;
 	}
 	
 	private void buildMemesList()
@@ -93,7 +93,7 @@ public class Chatbot
 	 */
 	public String getUserName()
 	{
-		return null;
+		return userName;
 	}
 	
 	/**
