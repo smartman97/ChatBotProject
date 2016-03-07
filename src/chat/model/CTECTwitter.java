2972c24a19dfafef51eca2d6fed2f439c8721978
Colm Laro <smartman97@gmail.com>
2016-03-07 09:09:04 -0700

    declared arraylist variables.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,18 @@
 package chat.model;
 
+import java.util.ArrayList;
+import twitter4j.*;
+
 public class CTECTwitter
 {
-
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
+	
+	
 }
